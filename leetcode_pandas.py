7718cb543c10eda890a3622ac02a11b2e56a1ea0
Tyler Correll <corrett10@gmail.com>
2023-12-14 11:47:27 -0600

    pandas leetcode

--- leetcode_pandas.py
+++ leetcode_pandas.py
@@ -5,3 +5,14 @@ def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
 
 def getDataframeSize(players: pd.DataFrame) -> List[int]:
     return list(players.shape)
+
+def selectFirstRows(employees: pd.DataFrame) -> pd.DataFrame:
+    rows = employees.head(3)
+    return rows
+
+def selectData(students: pd.DataFrame) -> pd.DataFrame:
+    return students[students.student_id == 101][["name","age"]]
+
+def createBonusColumn(employees: pd.DataFrame) -> pd.DataFrame:
+    employees['bonus'] = employees['salary'] * 2
+    return employees
