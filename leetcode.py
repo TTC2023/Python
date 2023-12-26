dc48b3acc79dcdd1c968a5cff135592108d23d19
Tyler Correll <corrett10@gmail.com>
2023-12-26 14:31:04 -0500

    leetcode

--- leetcode.py
+++ leetcode.py
@@ -663,6 +663,14 @@ def findWordsContaining(self, words: List[str], x: str) -> List[int]:
                     break
         return ans
 
+def countPairs(self, nums: List[int], target: int) -> int:
+        ans = 0
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i] + nums[j] < target:
+                    ans+=1
+        return ans
+
 
 
 
